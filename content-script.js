5eda2dbcda58ac0359a6d086d2318e49e0289fe8
Zekeriya <zekeriya054@gmail.com>
2025-01-03 11:53:30 +0300

    content-script.js

--- content-script.js
+++ content-script.js
@@ -1,4 +1,3 @@
-
 window.onfocus = () => {
     //video çalıştır
     vp=document.getElementById('video_html5_api');
@@ -9,29 +8,29 @@ window.onblur = () => {
     vp=document.getElementById('video_html5_api');
     vp.play();
 };
-
 vp=document.getElementById('video_html5_api');
+
 console.log(vp);
 vp.setAttribute('muted','true');
 vp.setAttribute('autoplay','true');
+
 var url=window.location.href;
-if(url=='https://www.oba.gov.tr/egitim/oynatma/uzman-ogretmenlik-egitim-programi-semineri-meb-personeli-286') {
-  var playList=document.getElementsByClassName("mdi mdi-check-circle progress-check-icon");
-  url=url+'/'+(3736+parseInt(playList.length));
-  console.log(url);
-}
-linkNo=url.substr(url.length-4,4);
+
+
+console.log(url);
+linkNo=url.substr(url.length-5,5);
 var link='content_'+linkNo;
 var sonrakiLink='content_'+(parseInt(linkNo)+1);
 var icon='icon_'+linkNo;
 console.log(icon);
-console.log(document.getElementById(icon).className);
+console.log(sonrakiLink);
+
 setInterval(() => {
 
   if(document.getElementsByClassName('vjs-remaining-time-display')[0].innerHTML=='-0:01') {
-     setTimeout(()=>{document.getElementById(sonrakiLink).click();}, 3000)
+    setTimeout(()=>{document.getElementById(sonrakiLink).click();}, 3000)
       
-  //  console.log(document.getElementsByClassName('vjs-remaining-time-display')[0].innerHTML);
+    //console.log(document.getElementsByClassName('vjs-remaining-time-display')[0].innerHTML);
 
   }
 }, 1000);
